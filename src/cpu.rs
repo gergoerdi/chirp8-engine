f4597711eefd92fc342f601211c1318bac8cb127
Gergo ERDI <gergo@erdi.hu>
2024-01-03 18:47:52 +0800

    Add compile-time parameters for some quirks

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,9 +2,25 @@ pub use prelude::*;
 pub use opcodes::*;
 pub use peripherals::Peripherals;
 
-pub struct CPU {
+use core::marker::PhantomData;
+
+pub trait Quirks {
+    const SHIFT_VY: bool;
+    const RESET_VF: bool;
+    const INCREMENT_PTR: bool;
+}
+
+pub struct DefaultQuirks;
+
+impl Quirks for DefaultQuirks {
+    const SHIFT_VY: bool = true;
+    const RESET_VF: bool = true;
+    const INCREMENT_PTR: bool = true;
+}
+
+pub struct CPU<Q: Quirks> {
     regs: [Byte; 16],
-    addr: Addr,
+    ptr: Addr,
     pc: Addr,
     stack: [Addr; 16],
     sp: usize,
@@ -12,12 +28,13 @@ pub struct CPU {
     timer: Byte,
     prev_key_state: u16,
     prev_key: Option<u8>,
+    quirks: PhantomData<Q>,
 }
 
-impl CPU {
-    pub const fn new() -> CPU {
+impl<Q: Quirks> CPU<Q> {
+    pub const fn new() -> Self {
         CPU{ regs : [0; 16],
-             addr: 0,
+             ptr: 0,
              pc: 0x200,
              stack: [0; 16],
              sp: 0,
@@ -25,6 +42,7 @@ impl CPU {
              timer: 0,
              prev_key_state: 0xffff,
              prev_key: None,
+             quirks: PhantomData,
         }
     }
 
@@ -43,9 +61,9 @@ impl CPU {
     fn arith(op: Arith, x: Byte, y: Byte) -> (Byte, Option<bool>) {
         match op {
             Arith::Load => (y, None),
-            Arith::Or => (x | y, Some(false)),
-            Arith::And => (x & y, Some(false)),
-            Arith::XOr => (x ^ y, Some(false)),
+            Arith::Or => (x | y, if Q::RESET_VF { Some(false) } else { None }),
+            Arith::And => (x & y, if Q::RESET_VF { Some(false) } else { None }),
+            Arith::XOr => (x ^ y, if Q::RESET_VF { Some(false) } else { None }),
             Arith::Add => {
                 let (z, f) = u8::overflowing_add(x, y);
                 (z, Some(f))
@@ -58,8 +76,14 @@ impl CPU {
                 let (z, f) = u8::overflowing_sub(y, x);
                 (z, Some(!f))
             },
-            Arith::ShiftL => (y << 1, Some(y & 0x80 != 0)),
-            Arith::ShiftR => (y >> 1, Some(y & 0x01 != 0))
+            Arith::ShiftL => {
+                let arg = if Q::SHIFT_VY { y } else { x };
+                (arg << 1, Some(arg & 0x80 != 0))
+            },
+            Arith::ShiftR => {
+                let arg = if Q::SHIFT_VY { y } else { x };
+                (arg >> 1, Some(arg & 0x01 != 0))
+            }
         }
     }
 
@@ -129,17 +153,17 @@ impl CPU {
             Op::Arith(op, vx, vy) => {
                 let x = self.regs[vx as usize];
                 let y = self.regs[vy as usize];
-                let (z, flag) = CPU::arith(op, x, y);
+                let (z, flag) = Self::arith(op, x, y);
                 self.regs[vx as usize] = z;
                 flag.map(|flag| { self.set_flag(flag); });
             },
             Op::LoadI(addr) => {
-                self.addr = addr;
+                self.ptr = addr;
             },
             Op::AddI(vx) => {
-                let addr = self.addr + self.regs[vx as usize] as u16;
+                let addr = self.ptr + self.regs[vx as usize] as u16;
                 self.set_flag(addr > 0x0fff);
-                self.addr = addr & 0x0fff;
+                self.ptr = addr & 0x0fff;
             },
             Op::GetTimer(vx) => {
                self.regs[vx as usize] = self.timer;
@@ -154,22 +178,28 @@ impl CPU {
                 self.regs[vx as usize] = self.next_random() & mask;
             },
             Op::Hex(vx) => {
-                self.addr = (self.regs[vx as usize] as u16 & 0x0f) << 3;
+                self.ptr = (self.regs[vx as usize] as u16 & 0x0f) << 3;
             },
             Op::StoreBCD(vx) => {
                 let x = self.regs[vx as usize];
-                io.write_ram(self.addr, x / 100);
-                io.write_ram(self.addr + 1, (x % 100) / 10);
-                io.write_ram(self.addr + 2, x % 10);
+                io.write_ram(self.ptr, x / 100);
+                io.write_ram(self.ptr + 1, (x % 100) / 10);
+                io.write_ram(self.ptr + 2, x % 10);
+                if Q::INCREMENT_PTR {
+                    self.ptr += 3;
+                }
             },
             Op::Save(vx) => {
                 for i in 0..vx as usize +1 {
-                    io.write_ram(self.addr + i as Addr, self.regs[i])
+                    io.write_ram(self.ptr + i as Addr, self.regs[i])
+                }
+                if Q::INCREMENT_PTR {
+                    self.ptr += 3;
                 }
             },
             Op::Restore(vx) => {
                 for i in 0..vx as usize +1 {
-                    self.regs[i] = io.read_ram(self.addr + i as Addr)
+                    self.regs[i] = io.read_ram(self.ptr + i as Addr)
                 }
             },
             Op::Draw(vx, vy, n) => {
@@ -177,7 +207,7 @@ impl CPU {
                 let xd = (self.regs[vx as usize]) & 0x3f;
                 for i in 0..n {
                     let yd = (self.regs[vy as usize] + i) & 0x1f;
-                    let dat = io.read_ram(self.addr + i as Addr);
+                    let dat = io.read_ram(self.ptr + i as Addr);
                     let row = ((dat as ScreenRow) << 56) >> xd;
 
                     let old_row = io.get_pixel_row(yd);
